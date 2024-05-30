a2322ba71a3ed0743f02cda383b846b4edb80be6
Nicholas Brabosa <barbosanicholas49@gmail.com>
2024-05-30 17:09:13 -0100

    add_tasks.txt

---
+++ tasks.py
@@ -0,0 +1,16 @@
+tasks = []
+
+def add_task(task):
+    tasks.append({"task": task, "completed": False})
+
+def main():
+    while True:
+        command = input("Comando (add/exit): ")
+        if command == "add":
+            task = input("Tarefa: ")
+            add_task(task)
+        elif command == "exit":
+            break
+
+if __name__ == "__main__":
+    main()
